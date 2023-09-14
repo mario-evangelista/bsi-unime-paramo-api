642874d578d8acf5dd137b0b29d6d6ee37653dde
mariojbe <mariojbe@gmail.com>
2023-09-14 16:20:57 -0300

    API Spring Boot

---
+++ src/test/java/br/edu/unime/paramo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unime.paramo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
